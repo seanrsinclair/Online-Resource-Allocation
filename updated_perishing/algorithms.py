168a6b4b9f9b93dadcb88a72561b0bfacd013e6d
Sean Sinclair <seanrsinclair@gmail.com>
2023-09-19 10:13:21 -0400

    update taub calculations

--- updated_perishing/algorithms.py
+++ updated_perishing/algorithms.py
@@ -5,6 +5,17 @@ DEBUG = False
 
 
 def fixed_threshold(size, resource_perish, max_budget, xopt, x_lower, n):
+    """
+        Fixed threshold allocation algorithm. Takes as input a fixed allocation value x_lower
+        to allocate across all timesteps until running out of budget
+
+        Returns:
+            waste: B - \sum_t N_t X_t
+            counterfactual envy: |xopt - x_t|
+            hindsight envy: \max_t |X_t - X_t'|
+            PUA: \sum_b fraction of b perished and un-allocated
+    """
+
     resource_dict = {}
     for b in range(max_budget): # Initializes dictionary of the various resources and allocation
         resource_dict[str(b)] = (0, resource_perish[b])
@@ -35,18 +46,23 @@ def fixed_threshold(size, resource_perish, max_budget, xopt, x_lower, n):
         hindsight_envy = 0
         counterfactual_envy = np.abs(xopt - x_lower)
         waste = max_budget - np.sum([resource_dict[str(b)][0] for b in range(max_budget)])
-    perish_un_alloc_vec = []
-    for b in range(max_budget):
-        if resource_dict[str(b)][1] < n:
-            perish_un_alloc_vec.append(1 - resource_dict[str(b)][0])
-        else:
-            perish_un_alloc_vec.append(0)
-    perish_un_allocate = np.sum(perish_un_alloc_vec)
-    
+
+    perish_un_allocate = np.sum([0 if resource_dict[str(b)][1] < n else (1 - resource_dict[str(b)][0]) for b in range(max_budget)])
+
     return perish_un_allocate, waste, counterfactual_envy, hindsight_envy
 
 
 def hope_guardrail_perish(size, resource_perish, max_budget, xopt, x_lower, n, Lt, demand_dist, perish_dist, n_upper):
+    """
+        Perishing guardrail allocation algorithm. Takes as input a fixed allocation value x_lower
+        as well as a value of L_t dictating X_upper that it oscilates between while allocating
+
+        Returns:
+            waste: B - \sum_t N_t X_t
+            counterfactual envy: |xopt - x_t|
+            hindsight envy: \max_t |X_t - X_t'|
+            PUA: \sum_b fraction of b perished and un-allocated
+    """
 
     resource_dict = {}
     for b in range(max_budget): # Initializes dictionary of the various resources and allocation
@@ -102,13 +118,81 @@ def hope_guardrail_perish(size, resource_perish, max_budget, xopt, x_lower, n, L
         counterfactual_envy = np.max([np.abs(xopt - x_lower), np.abs(xopt - x_upper)])
         waste = max_budget - np.sum([resource_dict[str(b)][0] for b in range(max_budget)])
 
-    perish_un_alloc_vec = []
-    for b in range(max_budget):
-        if resource_dict[str(b)][1] < n:
-            perish_un_alloc_vec.append(1 - resource_dict[str(b)][0])
+    perish_un_allocate = np.sum([0 if resource_dict[str(b)][1] < n else (1 - resource_dict[str(b)][0]) for b in range(max_budget)])
+    
+    return perish_un_allocate, waste, counterfactual_envy, hindsight_envy
+
+
+
+def hope_guardrail_original(size, resource_perish, max_budget, xopt, x_lower, n, Lt, demand_dist, perish_dist, n_upper):
+    """
+        Original guardrail allocation algorithm. Takes as input a value of L_t dictating X_upper that it 
+        oscilates between while allocating.
+
+        NOTE: 1) X_lower will be B / N_upper, regardless of perishing
+        2) Does not take into account perishing when checking to allocate x_upper.
+
+        Returns:
+            waste: B - \sum_t N_t X_t
+            counterfactual envy: |xopt - x_t|
+            hindsight envy: \max_t |X_t - X_t'|
+            PUA: \sum_b fraction of b perished and un-allocated
+    """
+    resource_dict = {}
+    for b in range(max_budget): # Initializes dictionary of the various resources and allocation
+        resource_dict[str(b)] = (0, resource_perish[b])
+    flag = False  # Flag for running out of budget
+    current_index = 0
+    
+    x_lower = max_budget / n_upper[0]
+
+    x_upper = x_lower + Lt
+    if DEBUG: print(f"X_lower: {x_lower}, X_upper: {x_upper}")
+    current_budget = max_budget
+
+    for t in range(n):
+        if t != n-1:
+            n_upper_future = n_upper[t+1]
         else:
-            perish_un_alloc_vec.append(0)
-    perish_un_allocate = np.sum(perish_un_alloc_vec)
+            n_upper_future = 0
+
+
+        if DEBUG: print(f"t: {t}, current_budget: {current_budget}, n_upper: {n_upper_future}")
+        if DEBUG: print(f"Check without perish: {size[t]*x_upper + n_upper_future*x_lower}")
+
+        if current_budget - size[t]*x_upper - n_upper_future*x_lower >= 0:
+            to_allocate = size[t] * x_upper
+            if DEBUG: print(f"t: {t} allocating x_upper")
+        else:
+            to_allocate = size[t] * x_lower
+        
+        alloc_tracker = 0
+        
+        for b in np.arange(current_index, max_budget):
+            (frac, perish_time) = resource_dict[str(b)]
+            current_index = b
+            if perish_time >= t: # perishing in the future from this round
+                alloc_amt = min(1 - frac, to_allocate - alloc_tracker)
+                alloc_tracker += alloc_amt
+                resource_dict[str(b)] = (frac+alloc_amt, perish_time)
+            if alloc_tracker >= to_allocate:
+                break
+                
+        # Update current budget
+        current_budget = max_budget - np.sum([1 if resource_dict[str(b)][1] <= t else resource_dict[str(b)][0] for b in range(max_budget)])
+        if alloc_tracker < to_allocate: # run out of resources
+            print(f'Out of resources at timestep: {t}')
+            flag = True
+            waste = 0
+            counterfactual_envy = np.abs(xopt - alloc_tracker / size[t])
+            hindsight_envy = np.abs(x_upper - alloc_tracker / size[t])
+
+    if flag == False: # did not run out of resources
+        hindsight_envy = np.abs(x_upper - x_lower)
+        counterfactual_envy = np.max([np.abs(xopt - x_lower), np.abs(xopt - x_upper)])
+        waste = max_budget - np.sum([resource_dict[str(b)][0] for b in range(max_budget)])
+
+    perish_un_allocate = np.sum([0 if resource_dict[str(b)][1] < n else (1 - resource_dict[str(b)][0]) for b in range(max_budget)])
     
     return perish_un_allocate, waste, counterfactual_envy, hindsight_envy
 
--- updated_perishing/helper.py
+++ updated_perishing/helper.py
@@ -2,11 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 EPS = 10e-3
+DEBUG = False
 
 def prob_confidence_interval(avg_mass, n):
-    # return mean + np.log(n) + np.sqrt(mean * np.log(n))
-    return avg_mass + (1/(2*avg_mass))*(np.log(n) + np.sqrt(np.log(n)**2 + 8 * avg_mass + np.log(n)))
+    """
+        Helper code for calculating a high probability upper estimate on
+        \sum_b \Ind{E} where avg_mass = \sum_b \Pr{E} and n is 1 / delta, high probability value
 
+        Mostly done to ease tuning confidence interval choices jointly
+
+    """
+    # return mean + np.log(n) + np.sqrt(mean * np.log(n))
+    if DEBUG: print(f"Avg Mass: {avg_mass}")
+    if DEBUG: print(f"Confidence Interval (1) : {avg_mass + (1/(2*avg_mass))*(np.log(n) + np.sqrt(np.log(n)**2 + 8 * avg_mass + np.log(n)))}, Confidence Interval (2): {avg_mass + np.sqrt(n*np.log(n))}, Confidence Interval (3): {avg_mass + np.sqrt(avg_mass * np.log(n))}")
+    min_value = np.min([avg_mass + np.sqrt(n*np.log(n)), avg_mass + np.sqrt(3*avg_mass * np.log(n)), avg_mass + (1/(2*avg_mass))*(np.log(n) + np.sqrt(np.log(n)**2 + 8 * avg_mass + np.log(n)))])
+    if DEBUG: print(f"Minimum Value: {min_value}")
+    return min_value
 def export_legend(legend, filename="LABEL_ONLY.pdf"):
     fig  = legend.figure
     fig.canvas.draw()
@@ -14,6 +25,9 @@ def export_legend(legend, filename="LABEL_ONLY.pdf"):
     fig.savefig(filename, dpi="figure", bbox_inches=bbox)
 
 def n_upper(demand_dist, n, num_iters = 1000):
+    """
+        Returns high probability estimate for N_{\geq t} for each t
+    """
     future_demand_samples = [[np.sum(demand_dist(n)[t:]) for t in range(n)] for _ in range(1000)] # gets samples of the total demand N_{> t}
     avg_demand = np.mean(future_demand_samples, axis=0)
     n_upper = avg_demand + np.asarray([np.sqrt(2*np.std(future_demand_samples, axis=0)[t]*(n-t)) for t in range(n)])
@@ -54,20 +68,22 @@ def x_lower_line_search(perish_dist, demand_dist, n, max_budget, n_upper):
     return x_lower
 
 
-def estimate_delta(x, perish_dist, demand_dist, n, max_budget, num_iters = 1000):
+def estimate_delta(x, perish_dist, demand_dist, n, max_budget, num_iters = 100):
     """
         Estimates Delta(X) = |{b : T_b < \tau_b(X)}| through Monte-Carlo simulations
         NOTE: This is simplifying \tau_b to not take into account the demands N_t
         ADJUST?
     """
+
     total_mass = 0
     for _ in range(num_iters):
         # sizes = demand_dist(n) # samples demands N_t
         resource_perish = np.asarray([perish_dist(b,n) for b in range(max_budget)]) # samples perishing times T_b
-        total_mass += np.sum([1 if resource_perish[b] < np.ceil(b/x) else 0 for b in range(max_budget)]) # checks whether
-            # resources perish before tau_b
+        total_mass += np.sum([1 if resource_perish[b] < np.minimum(n, np.ceil((b+1)/x)) else 0 for b in range(max_budget)]) # checks whether
+            # resources perish before tau_b (note that sigma(b) = b+1 due to python indexing starting from zero)
     avg_mass = (total_mass / num_iters) # takes expectation over the number of iterations
     return prob_confidence_interval(avg_mass, n) # returns an estimate for \overline{\Delta}
+
 
 def perish_future(t, current_index, resource_dict, x_lower, perish_dist, demand_dist, n, max_budget, num_iters = 100):
     """
@@ -76,15 +92,31 @@ def perish_future(t, current_index, resource_dict, x_lower, perish_dist, demand_
     """
 
     total_mass = 0
+
+    available_resources = []
+    for b in range(max_budget):
+        if (resource_dict[str(b)][1] >= t and resource_dict[str(b)][0] < 1):
+            available_resources.append(b)
+    if DEBUG: print(f"Available Resources: {available_resources}")
+        # based on the current resources B_t^{alg} returns the index of resources that (i) have not perished and (ii) have not been allocated fully yet
+        # (some weird rounding here but ignoring that)
+
     for _ in range(num_iters):
         # sizes = demand_dist(n) # samples demands N_t
 
         resource_perish = np.asarray([perish_dist(b,n) for b in range(max_budget)]) # samples a vector of perishing times
 
-        for b in np.arange(current_index, max_budget): # loops over the remaining resources in B_t^{alg}
-            if (resource_perish[b] < np.minimum(n, t + np.ceil((b - current_index)/x_lower))) and (resource_perish[b] >= t): # checks whether resource will perish before earliest it gets allocated
-                                            # note that the right hand side taub is also done in a state dependent way, since x_lower will be allocated starting at the current_index
-                total_mass += 1 # adds one to the total mass
+        total_mass += np.sum([1 if (resource_perish[b] < np.minimum(n, t + np.ceil((1+available_resources.index(b))/x_lower))) and (resource_perish[b] >= t) else 0 for b in available_resources])
+            # loop over remaining resources and checks whether resource will perish before earliest it gets allocated
+            # note that right hand side taub is done in state dependent way, since x_lower will be allocated starting at the current_index
+            # (+1) is done again due to indexing issues in python
+
+        # NOTE: OLD VERSION OF CALCULATION
+        # for b in available_resources: # loops over the remaining resources in B_t^{alg}
+        #     if (resource_perish[b] < np.minimum(n, t + np.ceil((b - current_index)/x_lower))) and (resource_perish[b] >= t): # checks whether resource will perish before earliest it gets allocated
+        #                                     # note that the right hand side taub is also done in a state dependent way, since x_lower will be allocated starting at the current_index
+        #         total_mass += 1 # adds one to the total mass
+
     avg_mass = (total_mass / num_iters)
     if avg_mass == 0:
         return 0
